f5cc300c5f5eb735fc88a86de42c213e95b673ee
Luis Camilo <luiscamilov@hotmail.com>
2025-04-06 13:05:36 -0500

    Configuration for reflex

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,40 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
   def new
-    
+    @task = Task.new
   end
   def create
-    
+    @task = Task.new(task_params)
+
+    if @task.save
+      
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-  def edit
-    
-  end
+  def edit; end
+
   def update
-    
+    if @task.update(task_params)
+
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
   def destroy
-    
+    @task.destroy
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resource :example, constraints: -> { Rails.env.development? }
   root "tasks#index"
   resources :tasks
 end
